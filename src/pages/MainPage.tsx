64b80cd0d1d5758c793471a54a8756d669a9ee0a
mark.avvakumov <maavvakumov@gmail.com>
2024-04-19 20:11:14 +0700

    Add Main page

---
+++ src/pages/MainPage.tsx
@@ -0,0 +1,12 @@
+import Card from '../components/Card'
+
+const MainPage = () => {
+  return (
+    <div className='grid gap-10'>
+      <Card />
+      <Card />
+    </div>
+  )
+}
+
+export default MainPage
