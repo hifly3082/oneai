3377c580234711f813aa5d915197c834b2472b17
mark.avvakumov <maavvakumov@gmail.com>
2024-04-19 20:10:41 +0700

    Add Footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+  return (
+    <footer className='flex flex-col justify-center items-center gap-[20px] bg-light rounded-t-[50px] py-8 font-sfpro text-[20px]'>
+      <p className='font-normal'>Get help at support@avgen.me</p>
+      <div className='font-extralight'>
+        <p>Copyright &169; 2024 Mook Ltd.</p>
+        <p>All rights reserved.</p>
+      </div>
+    </footer>
+  )
+}
+export default Footer
